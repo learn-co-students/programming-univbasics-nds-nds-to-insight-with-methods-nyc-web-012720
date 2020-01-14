fec551bcf13793d2a2e6297ad84568c2e43e95d6
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-14 01:10:09 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,6 +10,7 @@ require 'directors_database'
 # { directorOne => allTheMoneyTheyMade, ... }
 
 
+
 def gross_for_director(director_data)
   movie_total = 0 
   i = 0
@@ -17,49 +18,37 @@ def gross_for_director(director_data)
     movie_total += director_data[:movies][i][:worldwide_gross]
     i+=1 
   end
+  puts director_data.class
   movie_total
 end
 
-
-# def directors_totals(nds)
-
-#   # result = {}
-#   # i = 0
-#   # while i < directors_database.length do 
-#   #   result[directors_database[i][:name]] = gross_for_director(director_data)
-#   #   i += 1
-#   # end
-#   # return result
-  
-# end
-
-
-
-def directors_totals(nds)
-  result = {}
-  dir_name = []
-  total_gross = [] 
-  director_data = total_gross
-  
+def all_grosses(nds)
+  totals_arr = []
   i = 0 
   while i < nds.length do 
-    movie_total = 0 
-    dir_name << nds[i][:name]
+    total_gross = 0
     j = 0 
-      while j < nds[i][:movies].length
-      movie_total = movie_total + nds[i][:movies][j][:worldwide_gross]
-      j +=1 
+    while j < nds[i][:movies].length do 
+      total_gross = total_gross + nds[i][:movies][j][:worldwide_gross]
+      j += 1 
     end 
-    total_gross << movie_total
-    i+=1 
+    totals_arr << total_gross
+    i += 1 
   end
-  
-  k = 0 
-  while k < dir_name.length 
-    result[dir_name[k]] = total_gross[k]
-    k+=1 
-  end 
-  
-
-result
+totals_arr
 end
+
+def directors_totals(nds)
+
+  result = {}
+  i = 0
+  while i < nds.length do 
+    result[nds[i][:name]] = all_grosses(nds)[i]
+    i += 1
+  end
+ result
+  
+end
+
+
+
