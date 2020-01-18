fb030c87ff427277b83e504c33f2c857c44ab28a
April Escobar <aprilfaith.e@gmail.com>
2020-01-18 20:29:36 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -28,14 +28,10 @@ def directors_totals(nds)
     counter = 0
     while counter < nds.length do
       key = nds[counter][:name]
-      puts key
-
-      inner_counter = 0
       value = gross_for_director(nds[counter])
-
-    puts value
-    counter += 1
-    result[key] = value
+      counter += 1
+    
+      result[key] = value
     end
     
   result
