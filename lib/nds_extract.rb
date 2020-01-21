22ab302045a7480f1d1c01ab793943d58d910e17
Jared Fleming <jaredmf10@gmail.com>
2020-01-21 19:23:11 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -22,16 +22,11 @@ end
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
   director_index = 0
-  totals = {}
+  result = {}
   while director_index < nds.length do
-    director_name = nds[director_index][:name]
-    totals[director_name] = 0
-    movie_index = 0
-    while movie_index < nds[director_index][:movies].length do
-      totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
-      movie_index += 1
-    end
+    director = nds[director_index]
+    result[director[:name]] = gross_for_director(director)
     director_index += 1
   end
-  totals
+  result
 end
